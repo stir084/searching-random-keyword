799681b38a1fb1a544a959eb8e7a1bc738118297
junho <wnsgh1357@gmail.com>
2023-09-03 02:29:56 +0900

    feature: 부하 테스트

--- src/main/java/com/example/searchingrandomkeyword/TestController.java
+++ src/main/java/com/example/searchingrandomkeyword/TestController.java
@@ -1,6 +1,10 @@
 package com.example.searchingrandomkeyword;
 
 import com.github.javafaker.Faker;
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.exception.AddressNotFoundException;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CityResponse;
 import org.python.core.PyFunction;
 import org.python.core.PyInteger;
 import org.python.core.PyObject;
@@ -9,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Locale;
 
 @RestController
@@ -30,21 +37,43 @@ public class TestController {
     }
 
     @RequestMapping(value = "/test2", method = RequestMethod.GET)
-    public String getTest2(){
+    public String getTest2() throws IOException, GeoIp2Exception {
+
+        File uploads = new File("C:/GeoLite2-City.mmdb");
         Faker faker = new Faker();
 
-        String name = faker.name().fullName(); // Miss Samanta Schmidt
-        String firstName = faker.name().firstName(); // Emory
-        String lastName = faker.name().lastName(); // Barton
-        String ipaddr = faker.internet().ipV4Address();
-        String streetAddress = faker.address().streetAddress(); // 60018 Sawayn Brooks Suite 449
 
-        Faker faker2 = new Faker(new Locale("ko"));
 
-        String name2 = faker.name().fullName();
-        String firstName2 = faker.name().firstName();
-        String lastName2 = faker.name().lastName();
 
+        //	File database = new ClassPathResource("/src/main/resources/GeoLite2-City.mmdb").getFile();
+        DatabaseReader dbReader = new DatabaseReader.Builder(uploads)
+                .build();
+        long startTime = System.currentTimeMillis();
+
+
+        for (int i=0; i<1; i++){
+            String ipaddr = faker.internet().ipV4Address();
+            InetAddress ipAddress = InetAddress.getByName(ipaddr);
+            CityResponse response;
+            try {
+                response = dbReader.city(ipAddress);
+            } catch (AddressNotFoundException e) {
+                continue;
+            }
+
+
+            String countryName = response.getCountry().getName();
+            String cityName = response.getCity().getName();
+            String postal = response.getPostal().getCode();
+            String state = response.getLeastSpecificSubdivision().getName();
+           // System.out.println(countryName +"--"+cityName+"--"+postal+"--"+state);
+        }
+        long endTime = System.currentTimeMillis();
+
+        long timeElapsed = endTime - startTime;
+
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("Execution time in milliseconds: " + timeElapsed);
         return "test";
     }
 }
